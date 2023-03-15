f20d02019d82d233dbd431d8db8c3d82ba9fae50
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-15 10:55:22 -0600

    Complete custom hook test done already

--- src/hooks/useCounter/useCounter.test.ts
+++ src/hooks/useCounter/useCounter.test.ts
@@ -1,4 +1,5 @@
 import { renderHook } from "@testing-library/react"
+import { act } from "@testing-library/react-hooks"
 import { useCounter } from "."
 
 const makeSut = () => renderHook(() => useCounter())
@@ -8,5 +9,24 @@ describe("useCounter", () => {
     const {result} = makeSut()
 
     expect(result.current.counter).toBe(0)
+  })
+
+  test("It should increment to 1", () => {
+    const { result } = makeSut()
+    act(() => {
+      result.current.increase()
+    })
+
+    expect(result.current.counter).toBe(1)
+  })
+
+  test("It should decrement to -1", () => {
+    const { result } = makeSut()
+
+    act(() => {
+      result.current.decrease()
+    })
+
+    expect(result.current.counter).toBe(-1)
   })
 })
