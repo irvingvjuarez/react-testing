ed0e736429b9135df17ca4a04c61cb54cc4d8e8a
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-15 07:31:15 -0600

    event fired and tested correctly

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,11 +1,11 @@
 import { Button } from "./index"
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 
 const makeSut = (props: Partial<ButtonProps>) => {
   return render(
     <Button
       label="label"
-      onClick={() => console.log("Hello World")}
+      onClick={jest.fn()}
       {...props}
     />
   )
@@ -15,5 +15,14 @@ describe("<Button />", () => {
   test("Should render the label correctly", () => {
     const { getByText } = makeSut({ label: "My Button" })
     expect(getByText(/My Button/)).toBeInTheDocument()
+  })
+
+  test("Should call onClick successfully", () => {
+    const spy = jest.fn()
+    const { getByText } = makeSut({ onClick: spy })
+
+    fireEvent.click(getByText(/label/))
+
+    expect(spy).toHaveBeenCalled()
   })
 })
